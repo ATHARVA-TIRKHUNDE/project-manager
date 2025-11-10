3e848f5e313b942a42da1d9b0dd4deed31edf48a
ATHARVA-TIRKHUNDE <atharvatirkhunde@gmail.com>
2025-11-10 10:31:52 +0530

    Design project models & Schemas

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import app from "./app.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+  path: "../.env"
+})
+
+const port = process.env.PORT  
+
+app.listen(port, ()=>{
+  console.log(`App is listing on port http://localhost:${port}`)
+} )
---
+++ backend/src/models/taks.models.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableTaskStatuses, TaskStatusEnum } from "../utils/constants.js";
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxLength: [1000, "Description cannot be more than 2000 characters"],
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    assignedTo: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    assignedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      enum: AvailableTaskStatuses,
+      default: TaskStatusEnum.TODO,
+    },
+    attachments: {
+      type: [
+        {
+          url: String,
+          mimetype: String,
+          size: Number,
+        },
+      ],
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
+
+export const Taks = mongoose.model("Task", taskSchema);
