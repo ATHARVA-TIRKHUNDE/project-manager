d803479248fcc575332edbed386330ee33457645
ATHARVA-TIRKHUNDE <atharvatirkhunde@gmail.com>
2025-11-13 12:06:46 +0530

    feat(auth.middleware): Add the middleware to verify user

---
+++ backend/src/middlewares/auth.midlewares.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken"
+import { asyncHandler } from "../utils/async-handler.js"
+import { ApiError } from "../utils/api-error.js";
+import { User } from "../models/user.models.js";
+
+
+const verifyJwt = asyncHandler( async(req, res, next) => {
+  const token = 
+    req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
+
+  if(!token) {
+    throw new ApiError(401, "Unauthorized access") ;
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET,
+    );
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+    );
+
+    if(!user) {
+      throw new ApiError(401, "Invalid access token") ;
+    }
+
+    req.user = user ;
+    next();
+
+  } catch (error) {
+    throw new ApiError(401, "Invalid access token");
+  }
+
+});
+
+const validateProjectPermission = asyncHandler((req, res, next) => {
+
+});
+
+export {
+  verifyJwt,
+  validateProjectPermission
+}
+
