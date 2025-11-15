d0ea56d4e49c09b6ab5c871ea6fb0db1a3c9993c
ATHARVA-TIRKHUNDE <atharvatirkhunde@gmail.com>
2025-11-15 21:49:46 +0530

    feat(validator): Add projet validaor

--- backend/src/middlewares/validators.middlewares.js
+++ backend/src/middlewares/validators.middlewares.js
@@ -16,3 +16,35 @@ export const validate = (rea, res, next) => {
 
   throw new ApiError(422, ' Received data is not valid', extractedErrors)
 }
+
+export const validateProjectPermission = (roles = []) => {
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params
+
+    if (!projectId) {
+      throw new ApiError(400, 'project id is missing')
+    }
+
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id),
+    })
+
+    if (!project) {
+      throw new ApiError(400, 'project not found')
+    }
+
+    const givenRole = project?.role
+
+    req.user.role = givenRole
+
+    if (!roles.includes(givenRole)) {
+      throw new ApiError(
+        403,
+        'You do not have permission to perform this action'
+      )
+    }
+
+    next()
+  })
+}
