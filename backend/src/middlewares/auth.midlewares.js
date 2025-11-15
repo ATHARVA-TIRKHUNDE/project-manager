d584bd6acf23bb10e66e33bafbdad590532cb67e
ATHARVA-TIRKHUNDE <atharvatirkhunde@gmail.com>
2025-11-15 21:22:14 +0530

    refactor: refactor some changes in auth routes

--- backend/src/middlewares/auth.midlewares.js
+++ backend/src/middlewares/auth.midlewares.js
@@ -1,46 +1,35 @@
-import jwt from "jsonwebtoken"
-import { asyncHandler } from "../utils/async-handler.js"
-import { ApiError } from "../utils/api-error.js";
-import { User } from "../models/user.models.js";
+import jwt from 'jsonwebtoken'
+import { asyncHandler } from '../utils/async-handler.js'
+import { ApiError } from '../utils/api-error.js'
+import { User } from '../models/user.models.js'
 
+const verifyJwt = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header('Authorization')?.replace('Bearer ', '')
 
-const verifyJwt = asyncHandler( async(req, res, next) => {
-  const token = 
-    req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
-
-  if(!token) {
-    throw new ApiError(401, "Unauthorized access") ;
+  if (!token) {
+    throw new ApiError(401, 'Unauthorized access')
   }
 
   try {
-    const decodedToken = jwt.verify(
-      token,
-      process.env.ACCESS_TOKEN_SECRET,
-    );
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
     const user = await User.findById(decodedToken?._id).select(
-      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
-    );
+      '-password -refreshToken -emailVerificationToken -emailVerificationExpiry'
+    )
 
-    if(!user) {
-      throw new ApiError(401, "Invalid access token") ;
+    if (!user) {
+      throw new ApiError(401, 'Invalid access token')
     }
 
-    req.user = user ;
-    next();
-
+    req.user = user
+    next()
   } catch (error) {
-    throw new ApiError(401, "Invalid access token");
+    throw new ApiError(401, 'Invalid access token')
   }
+})
 
-});
+const validateProjectPermission = asyncHandler((req, res, next) => {})
 
-const validateProjectPermission = asyncHandler((req, res, next) => {
-
-});
-
-export {
-  verifyJwt,
-  validateProjectPermission
-}
-
+export { verifyJwt, validateProjectPermission }
